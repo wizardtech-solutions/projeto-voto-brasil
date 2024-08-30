a30f90bd47f255a2670b60566244efb60d73efeb
renato machado <renatomachado235@gmail.com>
2024-08-30 14:20:11 -0300

    atulaizacao 1.0

--- telaInicioVoto.mjs
+++ telaInicioVoto.mjs
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const formInicioVoto = document.querySelector('#formInicioVoto');
 
-    const inputs = document.querySelectorAll('.box_codigo');
-    inputs.forEach((input, index) => {
-      setTimeout(() => {
-        input.style.color = '#333333'; // Torna o texto visível
-      }, index * 250); // Ajusta o atraso para cada texto
-    });
-    
+  const inputs = document.querySelectorAll('.box_codigo');
+  inputs.forEach((input, index) => {
+    setTimeout(() => {
+      input.style.color = '#333333'; // Torna o texto visível
+    }, index * 200); // Ajusta o atraso para cada texto
+  });
+
+
+  setTimeout(() => {
+    formInicioVoto.submit();
+  }, 1500);
+
+
+
+
+
 });
 
 
